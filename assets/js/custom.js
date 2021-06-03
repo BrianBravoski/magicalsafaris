759557f6c99fb54aa8b0312a71c803a84ce6b0c0
BrianBravoski <brianbravoski28@gmail.com>
2021-06-03 22:04:44 +0300

    creating a form and validation

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,9 +3,9 @@
     var mainApp = {
 
         main_fun: function () {
-          
-       
-           
+
+
+
             $("a.preview").prettyPhoto({
                 social_tools: false
             });
@@ -32,13 +32,50 @@ var slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 3000); 
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000);
 }
+
+// takes tje 
+
+let username = document.querySelector("#username");
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+let finalUser = JSON.parse(localStorage.getItem('users'));
+
+
+
+function addUser(e) {
+
+    // checking that all the fields are filled as required
+    if (username.value === "" || email.value === "" || password.value === "") {
+        alert("Kindly complete all fields");
+        return false;
+    }
+
+
+    let allUsers = {
+        emailAll: email.value,
+        passwordAll: email.value,
+    }
+
+    let indUser = [];
+    if (localStorage.getItem('users') == null) {
+        indUser.push(allUsers);
+        localStorage.setItem('users', JSON.stringify(indUser))
+    } else {
+        indUser = JSON.parse(localStorage.getItem('users'));
+        indUser.push(allUsers);
+        localStorage.setItem('users', JSON.stringify(indUser))
+    }
+    console.log(indUser); alert('Registration completed, kindly log in')
+    window.location.href = "todologin.html"
+    e.preventDefault()
+};
